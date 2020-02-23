21b81620914f1bd58ddf776857851e34fdb75ec0
MinexAutomation <minex.automation@gmail.com>
2020-02-23 12:12:34 -0800

    Added IServiceCollection extensions.

--- source/R5T.Frisia.Standard/Code/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Frisia.Standard/Code/Extensions/IServiceCollectionExtensions.cs
@@ -14,34 +14,16 @@ namespace R5T.Frisia.Standard
     public static class IServiceCollectionExtensions
     {
         /// <summary>
-        /// Adds the <see cref="IAwsEc2ServerHostFriendlyNameProvider"/> service.
-        /// </summary>
-        public static IServiceCollection AddAwsEc2ServerHostFriendlyNameProvider(this IServiceCollection services, string hostFriendlyName)
-        {
-            services.AddInstanceAwsEc2ServerHostFriendlyNameProvider(hostFriendlyName);
-
-            return services;
-        }
-
-        /// <summary>
-        /// Adds the <see cref="IAwsEc2ServerHostFriendlyNameProvider"/> service.
-        /// </summary>
-        public static ServiceAction<IAwsEc2ServerHostFriendlyNameProvider> AddAwsEc2ServerHostFriendlyNameProviderAction(this IServiceCollection services, string hostFriendlyName)
-        {
-            var serviceAction = new ServiceAction<IAwsEc2ServerHostFriendlyNameProvider>(() => services.AddAwsEc2ServerHostFriendlyNameProvider(hostFriendlyName));
-            return serviceAction;
-        }
-
-        /// <summary>
         /// Adds the <see cref="IAwsEc2ServerSecretsProvider"/> service.
         /// </summary>
-        public static IServiceCollection AddAwsEc2ServerSecretsProvider(this IServiceCollection services, string hostFriendlyName)
+        public static IServiceCollection AddAwsEc2ServerSecretsProvider(this IServiceCollection services,
+            ServiceAction<IAwsEc2ServerHostFriendlyNameProvider> addAwsEc2ServerHostFriendlyNameProvider)
         {
             services.AddSuebiaAwsEc2ServerSecretsProvider(
                 services.AddHardCodedAwsEc2ServerSecretsFileNameProviderAction(),
                 services.AddSecretsFilePathProviderAction(),
                 services.AddJsonFileSerializationOperatorAction(),
-                services.AddAwsEc2ServerHostFriendlyNameProviderAction(hostFriendlyName));
+                addAwsEc2ServerHostFriendlyNameProvider);
 
             return services;
         }
@@ -49,9 +31,10 @@ namespace R5T.Frisia.Standard
         /// <summary>
         /// Adds the <see cref="IAwsEc2ServerSecretsProvider"/> service.
         /// </summary>
-        public static ServiceAction<IAwsEc2ServerSecretsProvider> AddAwsEc2ServerSecretsProviderAction(this IServiceCollection services, string hostFriendlyName)
+        public static ServiceAction<IAwsEc2ServerSecretsProvider> AddAwsEc2ServerSecretsProviderAction(this IServiceCollection services,
+            ServiceAction<IAwsEc2ServerHostFriendlyNameProvider> addAwsEc2ServerHostFriendlyNameProvider)
         {
-            var serviceAction = new ServiceAction<IAwsEc2ServerSecretsProvider>(() => services.AddAwsEc2ServerSecretsProvider(hostFriendlyName));
+            var serviceAction = new ServiceAction<IAwsEc2ServerSecretsProvider>(() => services.AddAwsEc2ServerSecretsProvider(addAwsEc2ServerHostFriendlyNameProvider));
             return serviceAction;
         }
     }
